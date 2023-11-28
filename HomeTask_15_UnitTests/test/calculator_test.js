39d32f5ae42fbbac89596867b4e215435f03847a
Svyatoslav Yakornov <yakornovml@gmail.com>
2023-11-28 19:28:20 +0300

    Modified eslint file + added 1 test throwing error

--- HomeTask_15_UnitTests/test/calculator_test.js
+++ HomeTask_15_UnitTests/test/calculator_test.js
@@ -9,22 +9,26 @@ describe('Calculator', () => {
   });
 
   it('should add numbers correctly', () => {
-    expect(calculator.add(2, 3, 4)).to.equal(9);
+    expect(calculator.add(43, 23, 65)).to.equal(131);
   });
 
   it('should multiply numbers correctly', () => {
-    expect(calculator.multiply(2, 3, 4)).to.equal(24);
+    expect(calculator.multiply(6, 34, 91)).to.equal(18564);
   });
 
   it('should subtract numbers correctly', () => {
-    expect(calculator.subtraction(5, 3)).to.equal(2);
+    expect(calculator.subtraction(515, 423)).to.equal(92);
   });
 
   it('should divide numbers correctly', () => {
-    expect(calculator.divide(10, 2)).to.equal(5);
+    expect(calculator.divide(450, 90)).to.equal(5);
   });
 
   it('should exponentiate numbers correctly', () => {
-    expect(calculator.exponentiation(3)).to.equal(9);
+    expect(calculator.exponentiation(13)).to.equal(169);
+  });
+  
+  it('should throw an error for division by zero', () => {
+    expect(() => calculator.divide(999, 0)).to.throw('Cannot divide by zero');
   });
 });
